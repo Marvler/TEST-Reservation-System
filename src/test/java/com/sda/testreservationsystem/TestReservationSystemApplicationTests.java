734c03a576aa33990ce42abb69be1a2855d8bc38
Wielopol <karolwiel@interia.pl>
2022-05-11 23:23:27 +0200

    Initial commit with Spring Maven project

---
+++ src/test/java/com/sda/testreservationsystem/TestReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sda.testreservationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestReservationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

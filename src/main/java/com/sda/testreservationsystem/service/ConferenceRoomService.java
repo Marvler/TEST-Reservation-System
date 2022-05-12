7b8d1d98282c93dbae1a7512fbd0514954f48655
Łukasz Lewandowski <lukastonie@gmail.com>
2022-05-12 16:41:13 +0200

    faature add Controllers(organization, reservation, conferenceroom).

---
+++ src/main/java/com/sda/testreservationsystem/service/ConferenceRoomService.java
@@ -0,0 +1,37 @@
+package com.sda.testreservationsystem.service;
+
+import com.sda.testreservationsystem.model.ConferenceRoom;
+import com.sda.testreservationsystem.model.Reservation;
+
+import java.util.List;
+
+public class ConferenceRoomService {
+
+
+    public List<Reservation> getAllConferenceRooms() {
+        // TO DO
+        return null;
+    }
+
+
+    public ConferenceRoom getConferenceRoomById(Long id) {
+        //TO DO
+        return null;
+    }
+
+    public ConferenceRoom createConferenceRoom(ConferenceRoom conferenceRoom) {
+        //TO DO
+        return null;
+    }
+
+
+    public ConferenceRoom updateConferenceRoom(Long id, ConferenceRoom conferenceRoom) {
+        //TO DO
+        return null;
+    }
+
+
+    public void deleteConferenceRoomById(Long id) {
+        //TO DO
+    }
+}
